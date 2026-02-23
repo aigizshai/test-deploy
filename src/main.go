5eec45d95622663ba01d96f74fad700fb6a4cede
aigizshai <aigizshai@gmail.com>
2026-02-23 19:09:49 +0400

    2

--- src/main.go
+++ src/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type response struct {
@@ -12,6 +17,58 @@ type response struct {
 
 func main() {
 	http.HandleFunc("/hello", helloHandler)
+
+	// 1. Загружаем .env файл (только для локальной разработки)
+	if err := godotenv.Load(); err != nil {
+		log.Println("Файл .env не найден, используем системные переменные")
+	}
+
+	// 2. Достаем строку подключения из окружения
+	connStr := os.Getenv("DB_URL")
+	if connStr == "" {
+		log.Fatal("Переменная DB_URL не задана")
+	}
+
+	// 3. Подключаемся к базе
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Ошибка подключения: %v", err)
+	}
+	defer db.Close()
+
+	// 4. Проверяем соединение
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Успешно подключились через секреты!")
+
+	// 5. Получаем список таблиц из схемы public
+	rows, err := db.Query(`
+			SELECT table_name 
+			FROM information_schema.tables 
+			WHERE table_schema = 'public' 
+			ORDER BY table_name
+		`)
+	if err != nil {
+		log.Fatalf("Ошибка запроса списка таблиц: %v", err)
+	}
+	defer rows.Close()
+
+	// 6. Выводим названия таблиц
+	log.Println("Список таблиц в базе pioneer (схема public):")
+	for rows.Next() {
+		var tableName string
+		if err := rows.Scan(&tableName); err != nil {
+			log.Fatal(err)
+		}
+		log.Println(" -", tableName)
+	}
+
+	// 7. Проверяем на ошибки после итерации
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
 
 	log.Println("Сервер запущен на http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
